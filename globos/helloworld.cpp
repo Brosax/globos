1c5def64f442fb1a7cf70f2c7e9b81c2e4d8c3e9
xiangmin fan <xiangminfan@gmail.com>
2024-10-03 18:04:42 +0200

    cin funtion added

--- globos/helloworld.cpp
+++ globos/helloworld.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
-    
-cout<<"nihao~";
+
+string a;
+cout<<"nihao~\n";
+cout<<"introduce algo:";
+cin>>a;
 cout<<"h o l a";
 cout<<"h o l a";
 cout<<"hello world\n";
-cout<<"in not fine";
+cout<<"in not fine\n";
+
+cout<<a;
 return 0;
 }
